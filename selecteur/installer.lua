89ec11777c6d0fe23ba9d17b5fca15058462a817
BuildTools <unconfigured@null.spigotmc.org>
2017-10-21 19:47:33 +0200

    First

--- selecteur/installer.lua
+++ selecteur/installer.lua
@@ -9,8 +9,8 @@ local filelist = {
 	'logiciel/data/as_en_haut.lua';
 	'logiciel/data/error.lua';
 	'logiciel/data/panne.lua';
-	'Templates/accueil.lua';
-	'Templates/background2.lua';
+	'templates/accueil.lua';
+	'templates/background2.lua';
 	'resettotal.lua';
 }
 
